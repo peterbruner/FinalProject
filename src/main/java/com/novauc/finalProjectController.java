e8e2401d832d1a1d4011058813df730f2d6bbc1f
D'Angelo Joyce <dangelomjoyce@gmail.com>
2017-04-12 18:57:19 -0400

    added scraping method that converts xml to json

--- src/main/java/com/novauc/finalProjectController.java
+++ src/main/java/com/novauc/finalProjectController.java
@@ -1,43 +1,102 @@
 package com.novauc;
 
-import jdk.nashorn.internal.parser.JSONParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
 
 /**
  * Created by dangelojoyce on 4/10/17.
  */
 @RestController
 public class finalProjectController implements FinalProjectInterface {
-    public String eStore ()throws IOException, JAXBException {
+    public String eStore() throws IOException, JAXBException {
 
-            Store store = new Store();
-            String uri =
-                    "\"http://api.walmartlabs.com/v1/stores?format=json&zip=20744&apiKey=c3exxssx4eme5j56s5zk7xg7\"";
-            URL url = new URL(uri);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Accept", "application/json");
+        Store store = new Store();
+        String uri =
+                "http://api.walmartlabs.com/v1/stores?format=json&zip=20744&apiKey=c3exxssx4eme5j56s5zk7xg7";
+        URL url = new URL(uri);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/json");
 
-            JSONParser jp = JSONParser.class.cast(store);
-            InputStream json = connection.getInputStream();
+       // JsonParser jsonParser = jsonParser()
 
+        InputStream json = connection.getInputStream();
 
-
-            System.out.println("This is:" + json);
-            System.out.println("This is" + jp);
-
-            connection.disconnect();
+        connection.disconnect();
 
 
-            return "";
-        }
+        return "";
     }
+    @RequestMapping(path = "/walmart")
+    public String walmartTest() {
+        try {
+            return eStore();
+        } catch (Exception e){
+            System.out.println("Houston had a problem @: ");
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    @RequestMapping(path = "/iphone")
+    public String test() throws IOException {
+            return scrape();
+    }
+
+    public String scrape() throws IOException {
+        String searchQuery = "bike";
+
+        WebClient client = new WebClient();
+        client.getOptions().setCssEnabled(false);
+        client.getOptions().setJavaScriptEnabled(false);
+        String searchUrl = "https://newyork.craigslist.org/search/sss?sort=rel&query=" + URLEncoder.encode(searchQuery, "UTF-8");
+        HtmlPage page = client.getPage(searchUrl);
+
+  //      System.out.println(page.asXml());
+        //page = page.asXml();
+
+        List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//p[@class='result-info']");
+        if (items.isEmpty()) {
+            System.out.println("No items found !");
+        } else {
+            for (HtmlElement htmlItem : items) {
+
+                HtmlAnchor itemAnchor = htmlItem.getFirstByXPath(".//a");
+
+                HtmlElement spanPrice = htmlItem.getFirstByXPath(".//span[@class='result-price']");
+
+                String itemPrice = spanPrice == null ? "0.0" : spanPrice.asText() ;
+
+                Item item = new Item();
+                item.setTitle(itemAnchor.asText());
+                item.setUrl( "https://newyork.craigslist.org" +
+                        itemAnchor.getHrefAttribute());
+                item.setPrice(new BigDecimal(itemPrice.replace("$", "")));
+
+                ObjectMapper mapper = new ObjectMapper();
+                String jsonString = mapper.writeValueAsString(item) ;
+                System.out.println(jsonString);
+                    }
+                }
+
+        return "";
+    }
+}
+
 
 
 
